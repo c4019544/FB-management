8304d4e2ef2a0d23365c6aa526f38b071001e102
PlanetAtomiC <jakeharrison374@gmail.com>
2025-03-21 09:17:45 +0000

    JH

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Sign-in Page</title>
-</head>
-<body>
-    <p></p>
-</body>
-</html>
-=======
-3
->>>>>>> a303161fb837397da14eaca405cd77650d452907
+<form action="../actions/login_user.php" method="post">
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit" value="submit">Login</button>
+</form>
+<style>
+        form { font-family: Arial, sans-serif; text-align: center; }
+        input{
+            width: 100%;
+            padding: 12px;
+            margin: 20px 0;
+            border: 2px solid black;
+            border-radius: 10px;
+        }
+        button {
+            padding:5px 15px; 
+            background:#ccc; 
+            border:0 none;
+            cursor:pointer;
+            -webkit-border-radius: 5px;
+            border-radius: 5px;
+        }
+    </style>
