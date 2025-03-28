0b0dbbaf4322abf81472aa409acbceb04172e038
PlanetAtomiC <jakeharrison374@gmail.com>
2025-03-28 10:09:47 +0000

    JH

---
+++ TeamManager/Sidebar.php
@@ -0,0 +1,13 @@
+<div class="sidebar">
+    <h2>Team Manager</h2>
+    <ul>
+        <li><a href="TeamManager.php">Dashboard</a></li>
+        <li><a href="TeamRoster.php">Team Roster</a></li>
+        <li><a href="UpcomingMatches.php">Upcoming Matches</a></li>
+        <li><a href="TrainingSchedule.php">Training Schedule</a></li>
+        <li><a href="PlayerManagment.php">Player Management</a></li>
+        <li><a href="MatchSettings.php">Match Settings</a></li>
+        <li><a href="Messages.php">Messages</a></li>
+        <li><a href="Calander.php">Calendar</a></li>
+    </ul>
+</div>
