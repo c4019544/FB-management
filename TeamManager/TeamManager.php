bc4be9be86c6c79fb94cfcb1d686daf933984715
c4019544 <zinthanwarat89@gmail.com>
2025-04-02 11:37:01 +0100

    zz styles so far

--- TeamManager/TeamManager.php
+++ TeamManager/TeamManager.php
@@ -1,4 +1,4 @@
-
+<!-- Dashboard for Team Manager -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team Manager</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link rel="stylesheet" href="../styles/style.css">
     <style>
         #chartContainer {
             width: 50%; 
@@ -16,71 +17,6 @@
         #StatsChart a{
             width: 100% !important; 
             height: 400px;          
-        }
-
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            height: 100vh;
-        }
-
-        .sidebar {
-            width: 250px;
-            background-color: #153C57;
-            color: white;
-            position: fixed;
-            top: 0;
-            left: 0;
-            bottom: 0;
-            padding-top: 20px;
-            padding-left: 10px;
-        }
-
-        .sidebar h2 {
-            color: #ecf0f1;
-            font-size: 24px;
-            margin-bottom: 30px;
-        }
-
-        .sidebar ul {
-            list-style: none;
-            padding: 0;
-        }
-
-        .sidebar ul li {
-            padding: 15px;
-            text-align: left;
-            margin: 10px 0;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        .sidebar ul li:hover {
-            background-color: rgb(32, 88, 129);
-        }
-
-        .content {
-            margin-left: 250px;
-            padding: 20px;
-            width: 100%;
-        }
-
-        .content header {
-            background-color: #ecf0f1;
-            padding: 20px;
-            text-align: center;
-            margin-bottom: 20px;
-        }
-
-        .content header h1 {
-            margin: 0;
-        }
-
-        a {
-            color: white;
-            text-decoration: none;
         }
     </style>
 </head>
