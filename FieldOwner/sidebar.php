8067d44c974937fa0ed63bcbeedff50ac0bdae83
alexadeyemo <alexadeyemo03@gmail.com>
2025-04-02 10:58:11 +0100

    FO 2

--- FieldOwner/sidebar.php
+++ FieldOwner/sidebar.php
@@ -4,10 +4,8 @@
         <li><a href="FODashboard.php">Dashboard</a></li>
         <li><a href="Field Overview.php">Field Overview</a></li>
         <li><a href="Bookings.php">Bookings</a></li>
-        <li><a href="TrainingSchedule.php">Training Schedule</a></li>
-        <li><a href="PlayerManagment.php">Player Management</a></li>
-        <li><a href="MatchSettings.php">Match Settings</a></li>
-        <li><a href="Messages.php">Messages</a></li>
-        <li><a href="Calander.php">Calendar</a></li>
+        <li><a href="FieldMaintenance.php">Field Maintenance</a></li>
+        <li><a href="MatchSchedule.php">Match Schedule</a></li>
+        <li><a href="Notifications.php">Notifications</a></li>
     </ul>
 </div>
