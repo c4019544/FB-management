dfcac7ccae7408be73405ae89bdedb21768bf92b
c4019544 <zinthanwarat89@gmail.com>
2025-04-07 11:49:52 +0100

    zz table matches css

--- TeamManager/Calander.php
+++ TeamManager/Calander.php
@@ -21,27 +21,56 @@ if (!$results) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Match Calendar</title>
     <link rel="stylesheet" href="../styles/style.css">
+    <!-- Link to Calendar API/ FullCalendar -->
+    <link href='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/main.min.css' rel='stylesheet' />
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/main.min.js'></script>
 
 </head>
+<style>
+#upcoming-matches-section{
+    margin-left: 20px;
+}
+
+#upcoming-match-table{
+    border: 2px solid black;
+    border-collapse: separate;
+}
+
+#upcoming-match-table tr th{
+    color: white;
+    background-color:#2d4458;
+    padding: 10px 20px;
+}
+
+#upcoming-match-table tr td{
+    padding: 10px 20px;
+}
+</style>
+
+
 <body>
     <?php include 'sidebar.php'; ?>
     <div class="content">
         <header>
             <h1>Match Calendar</h1>
         </header>
-        <h2>Upcoming Matches</h2>
-        <table>
-            <tr>
-                <th>Teams</th>
-                <th>Date</th>
-            </tr>
-            <?php while ($row = $results->fetchArray(SQLITE3_ASSOC)): ?>
-            <tr>
-                <td><?php echo htmlspecialchars($row['team1']) . " vs " . htmlspecialchars($row['team2']); ?></td>
-                <td><?php echo htmlspecialchars($row['Match_Date']); ?></td>
-            </tr>
-            <?php endwhile; ?>
-        </table>
+
+        <section id="upcoming-matches-section">
+            <h2>Upcoming Matches</h2>
+            <table id="upcoming-match-table">
+                <tr>
+                    <th>Teams</th>
+                    <th>Date</th>
+                </tr>
+                <?php while ($row = $results->fetchArray(SQLITE3_ASSOC)): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($row['team1']) . " vs " . htmlspecialchars($row['team2']); ?></td>
+                    <td><?php echo htmlspecialchars($row['Match_Date']); ?></td>
+                </tr>
+                <?php endwhile; ?>
+            </table>
+        </section>
+        
     </div>
 
 </body>
