b1555d3c87a886413d6e5c44a4d3c340ed363591
jamieanderson7005 <jamiesanderson144@gmail.com>
2025-04-07 11:48:22 +0100

    JA

--- Admin/AdminCreateUserPage.php
+++ Admin/AdminCreateUserPage.php
@@ -15,3 +15,18 @@
 
     <div class="main">
         <form action="AdminCreateUserRecord.php" method="post">
+            <input type="text" id="user_id" name="user_id" placeholder="User ID" required>
+            <input type="text" id="firstName" name="firstName" placeholder="First Name" required>
+            <input type="text" id="lastName" name="lastName" placeholder="Last Name" required>
+            <input type="text" id="dob" name="dob" placeholder="Date Of Birth (EG:1990-05-22)" required>
+            <input type="text" id="email" name="email" placeholder="Email Address" required>
+            <input type="text" id="telNo" name="telNo" placeholder="Phone Number" required>
+            <input type="text" id="role" name="role" placeholder="Role (Manager, Referee, Player, Field Owner)" required>
+            <input type="text" id="gender" name="gender" placeholder="Gender" required>
+            <input type="text" id="password" name="password" placeholder="Default Password" required>
+
+            <button type="submit">Create User</button>
+        </form>
+    </div>
+</body>
+</html>
