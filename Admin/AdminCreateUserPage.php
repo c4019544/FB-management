ab0c04b81d3b5c13be49bcb17f46ce44b2c1e880
jamieanderson7005 <jamiesanderson144@gmail.com>
2025-04-07 11:15:38 +0100

    JA

---
+++ Admin/AdminCreateUserPage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create New User</title>
+</head>
+
+<body>
+    <?php
+    include("sidebar2.php");
+    ?>
+    <div> <h2 class="centered_header">Create New User</h2></div>
+
+    <div class="main">
+        <form action="AdminCreateUserRecord.php" method="post">
