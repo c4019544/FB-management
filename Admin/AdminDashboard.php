2c3a2834e7b9ff38985862c17710457431b47cf4
jamieanderson7005 <jamiesanderson144@gmail.com>
2025-04-07 10:55:09 +0100

    JA

--- Admin/AdminDashboard.php
+++ Admin/AdminDashboard.php
@@ -45,3 +45,34 @@
         $db2->close();
         ?>
     </div>
+
+    <div>
+        <?php
+        $db3 = new SQLite3('C:\Users\jamie\OneDrive\Documents\xampp\htdocs\FB-management\fb_managment_system.db');
+        $query3 = "SELECT t1.team_name AS home_team, t2.team_name AS away_team, f.date From Fixtures f
+                            JOIN Team t1 ON f.home_team_id = t1.Team_ID
+                            Join Team t2 ON f.away_team_id = t2.Team_ID
+                            ORDER BY f.date ASC";
+        $result3 = $db3->query($query3);
+
+        if ($result3) {
+            echo"<h2>Upcoming Fixtures</h2>";
+            echo"<table border='1'>";
+            echo"<tr><th>Teams</th><th>Match Date</th></tr>";
+
+            while ($row = $result3->fetchArray(SQLITE3_ASSOC)) {
+                $teams = $row['home_team'] . " vs " . $row['away_team'];
+
+                echo"<tr>";
+                echo"<td>" . $teams . "</td>";
+                echo"<td>" . $row['date'] . "</td>";
+                echo"</tr>";
+            }
+
+            echo"</table>";
+        } else {
+            echo"No Fixtures Found";
+        }
+
+        $db3->close();
+        ?>
