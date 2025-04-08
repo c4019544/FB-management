519cbf87d67a2ac4903b78a1f6f859891d1a3ccd
c4019544 <zinthanwarat89@gmail.com>
2025-04-08 20:21:43 +0100

    z jf

--- TeamManager/Calander.php
+++ TeamManager/Calander.php
@@ -53,7 +53,7 @@ if (!$results) {
         <br>
     </div>
 
-    <footer>
+    <footer class="footer">
         <p>goikontech@gmail.com</p>
         <a href="#">Terms of use</a>
         <a href="#">Support</a>
