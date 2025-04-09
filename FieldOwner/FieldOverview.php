5047ad87ec118a787ca07b93ff19427d1d66469b
Alex Adeyemo <alexadeyemo03@gmail.com>
2025-04-09 10:23:00 +0100

    field overview
    fixed field overview table

--- FieldOwner/FieldOverview.php
+++ FieldOwner/FieldOverview.php
@@ -9,104 +9,93 @@
 </head>
 
 <body>
-    <section class="col-1">
-        <?php include 'Sidebar.php'; ?>
-    </section>
+
+    <?php include 'Sidebar.php'; ?>
+
+    <div class="content">
+        <header>
+            <h1>Field Overview</h1>
+        </header>
 
 
-    <section class="col-11">
-        <div class="bg-image h-100" style="background-color: #f5f7fa;">
-            <div class="mask d-flex align-items-center h-100">
-            <div class="container">
-                <div class="row justify-content-center">
-                <div class="col">
-                    <div class="card">
-                    <div class="card-body p-0">
-                        <div class="table-responsive table-scroll" data-mdb-perfect-scrollbar="true" style="position: relative;">
+        <section class="col">
+            <div class="bg-image h-100" style="background-color: #f5f7fa;">
+                <div class="mask d-flex align-items-center h-100">
+                <div class="container">
+                    <div class="row justify-content-center">
+                    <div class="col">
+                        <div class="card">
+                        <div class="card-body p-0">
+                            <div class="table-responsive table-scroll field-table" data-mdb-perfect-scrollbar="true" style="position: relative;">
 
-                        <?php
-                            // Connect to the SQLite database
-                            $db = new SQLite3(filename: '../fb_managment_system.db');
+                            <?php
+                                // Connect to the SQLite database
+                                $db = new SQLite3(filename: '../fb_managment_system.db');
 
-                            // Query to get data from a table
-                            $query = 'SELECT Field_name, Field_Capacity, Address_Line1 || ", " || City || ", " || Country || ", " || Postcode as "Address", GLT, VAR, View_Screens, Press_Box, Chg_Hour FROM Field WHERE FieldOwner_ID="FEO002"'; // Replace 'your_table' with the name of your table
-                            $result = $db->query($query);
+                                // Query to get data from a table
+                                $query = 'SELECT Field_name, Field_Capacity, Address_Line1 || ", " || City || ", " || Country || ", " || Postcode as "Address", GLT, VAR, View_Screens, Press_Box, Chg_Hour FROM Field WHERE FieldOwner_ID="FEO002"'; // Replace 'your_table' with the name of your table
+                                $result = $db->query($query);
 
-                            if (!$result) {
-                                echo "Error fetching data.";
-                            } else {
-                                // Start the HTML table
-                                echo "<table class='table table-striped mb-0'>
-                            <thead style='background-color: #002d72;'>
-                                <tr>
-                                    <th scope='col'>Field Name</th>
-                                    <th scope='col'>Capacity</th>
-                                    <th scope='col'>Address</th>
-                                    <th scope='col'>GLT</th>
-                                    <th scope='col'>VAR</th>
-                                    <th scope='col'>View Screens</th>
-                                    <th scope='col'>Press Box</th>
-                                    <th scope='col'>Chg per Hour</th>
-                                </tr>
-                            </thead>";
+                                if (!$result) {
+                                    echo "Error fetching data.";
+                                } else {
+                                    // Start the HTML table
+                                    echo "<table class='table table-striped mb-0'>
+                                <thead style='background-color: #002d72;'>
+                                    <tr>
+                                        <th scope='col'>Field Name</th>
+                                        <th scope='col'>Capacity</th>
+                                        <th scope='col'>Address</th>
+                                        <th scope='col'>GLT</th>
+                                        <th scope='col'>VAR</th>
+                                        <th scope='col'>View Screens</th>
+                                        <th scope='col'>Press Box</th>
+                                        <th scope='col'>Chg per Hour</th>
+                                    </tr>
+                                </thead>";
 
-                                // Loop through the query result and display the data in table rows
-                                while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-                                    echo "<tr>
-                                            <td>" . htmlspecialchars($row['Field_name']) . "</td>
-                                            <td>" . htmlspecialchars($row['Field_Capacity']) . "</td>
-                                            <td>" . htmlspecialchars($row['Address']) . "</td>
-                                            <td>" . htmlspecialchars($row['GLT']) . "</td>
-                                            <td>" . htmlspecialchars($row['VAR']) . "</td>
-                                            <td>" . htmlspecialchars($row['View_Screens']) . "</td>
-                                            <td>" . htmlspecialchars($row['Press_Box']) . "</td>
-                                            <td>" . htmlspecialchars($row['Chg_Hour']) . "</td>
-                                        </tr>";
+                                    // Loop through the query result and display the data in table rows
+                                    while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+                                        echo "<tr>
+                                                <td>" . htmlspecialchars($row['Field_name']) . "</td>
+                                                <td>" . htmlspecialchars($row['Field_Capacity']) . "</td>
+                                                <td>" . htmlspecialchars($row['Address']) . "</td>
+                                                <td>" . htmlspecialchars($row['GLT']) . "</td>
+                                                <td>" . htmlspecialchars($row['VAR']) . "</td>
+                                                <td>" . htmlspecialchars($row['View_Screens']) . "</td>
+                                                <td>" . htmlspecialchars($row['Press_Box']) . "</td>
+                                                <td>" . htmlspecialchars($row['Chg_Hour']) . "</td>
+                                            </tr>";
+                                    }
+
+                                    // End the table
+                                    echo "</table>";
                                 }
 
-                                // End the table
-                                echo "</table>";
-                            }
-
-                            // Close the database connection
-                            $db->close();
-                        ?>
+                                // Close the database connection
+                                $db->close();
+                            ?>
 
 
-
-                        
-                            <!-- <tbody>
-                                <tr>
-                                    <td>New Trafford</td>
-                                    <td>74000</td>
-                                    <td>TestData 1</td>
-                                    <td>YES</td>
-                                    <td>YES</td>
-                                    <td>0</td>
-                                    <td>YES</td>
-                                    <td>450</td>
-                                </tr>
-                                <tr>
-                                    <td>Sandygate</td>
-                                    <td>700</td>
-                                    <td>TestData 2</td>
-                                    <td>NO</td>
-                                    <td>NO</td>
-                                    <td>0</td>
-                                    <td>NO</td>
-                                    <td>110</td>
-                                </tr>
-                            </tbody> -->
-                        </table>
+                            </table>
+                            </div>
+                        </div>
                         </div>
                     </div>
                     </div>
                 </div>
                 </div>
             </div>
-            </div>
-        </div>
-    </section>
+        </section>
+    </div>
+
+    
 
 </body>
 </html>
+
+<style>
+/* .field-table{
+    margin:600px 100px;
+} */
+</style>
