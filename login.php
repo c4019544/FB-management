f5f9b4580357660529ef97b6340d31ec29074724
Alex Adeyemo <alexadeyemo03@gmail.com>
2025-04-11 18:35:45 +0100

    fo and login
    removed some comments from login.php
    fully completed the backend for field overview page.

--- login.php
+++ login.php
@@ -2,31 +2,30 @@
 
 session_start();
 
-// Check if the form was submitted
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get the form data
-    $username = $_POST['email'];
+
+    $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Connect to SQLite database
+
     $db = new SQLite3('fb_managment_system.db');
 
-    // Prepare the SQL query to fetch user data
+
     $stmt = $db->prepare('SELECT * FROM Users WHERE Email_Address = :email');
-    $stmt->bindValue(':email', $username, SQLITE3_TEXT);
+    $stmt->bindValue(':email', $email, SQLITE3_TEXT);
     $result = $stmt->execute();
 
-    // Check if the user exists
+
     $user = $result->fetchArray(SQLITE3_ASSOC);
 
     if ($user) {
-        // User found, verify the password
         if ($password === $user['Password']) {
-            // Password is correct, store staff info
-            $_SESSION['username'] = $username;
-            $_SESSION['Role'] = $user['Role']; // Store the user type in session
 
-            // Redirect based on user type
+            $_SESSION['email'] = $email;
+            $_SESSION['Role'] = $user['Role'];
+
+
             if ($user['Role'] === 'Admin') {
                 header('Location: Admin/AdminDashboard.php');
             } elseif ($user['Role'] === 'Manager') {
