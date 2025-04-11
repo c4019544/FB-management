98472430973360dda79e65222d6f79f71add3086
Alex Adeyemo <alexadeyemo03@gmail.com>
2025-04-11 10:41:44 +0100

    fo dashboard
    asf

--- FieldOwner/FOwnerDashboard.php
+++ FieldOwner/FOwnerDashboard.php
@@ -5,19 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | Field Owner</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="../styles/style.css"> -->
+    <link rel="stylesheet" href="../styles/style.css">
+    <style>
+        #chartContainer {
+            width: 50%; 
+            margin: 0 auto; 
+            padding-top: 20px; 
+        }
+
+        #StatsChart a{
+            width: 100% !important; 
+            height: 400px;          
+        }
+    </style>
 </head>
 
 <body>
     <?php include 'Sidebar.php'; ?>
 
-<!--        
+       
     <div class="content">
         <header>
             <h1>Field Owner Dashboard</h1>
         </header>
     </div>
-    -->
+   
     <script>
         var mySidebar = document.querySelector('Sidebar')
         var sidebar = new coreui.Sidebar(mySidebar)
