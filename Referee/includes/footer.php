d995bee9523ce12455835bb9665e20736113df47
Abdur765 <abrsubhan@gmail.com>
2025-04-28 12:47:54 +0100

    AR

--- Referee/includes/footer.php
+++ Referee/includes/footer.php
@@ -1,9 +1,5 @@
 <footer>
-<<<<<<< HEAD
     <p>&copy; <?php echo date("Y"); ?> Match Management System</p>
-=======
-    <p>&copy; <?php echo date("Y"); ?> Football Management System</p>
->>>>>>> jamie
     <p>All rights reserved.</p>
     <p>goikontech@gmail.com</p>
     <p>term of use</p>
